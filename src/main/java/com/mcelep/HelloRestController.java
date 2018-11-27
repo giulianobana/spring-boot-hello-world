23375c142c7d8fa7b66830c8ce5c50b0f0223ee9
mcelep <admin@mcelep.ch>
2018-11-27 22:04:20 +0100

    add greetign msg based on ENV

--- src/main/java/com/mcelep/HelloRestController.java
+++ src/main/java/com/mcelep/HelloRestController.java
@@ -4,14 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by XTL on 8/14/2014.
- */
 @RestController
 public class HelloRestController {
 
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String index() {
-        return "Hello World!!";
+        return "Hello World " + readEnvironmentVariable() + " !";
+    }
+
+    private String readEnvironmentVariable(){
+        String result = System.getenv("HELLO_WORLD_MSG");
+        return result==null?"":result;
     }
 }
